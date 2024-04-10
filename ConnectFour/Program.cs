7ee107f83eedf75dd5d0d040b0475c4eec24a5b3
Lovelycunanan <149105959+Lovelycunanan@users.noreply.github.com>
2024-04-09 23:20:26 -0600

    Conditions Program.cs

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -18,17 +18,84 @@ namespace ConnectFour
     
     private void DisplayBoard()
     {
-        
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Cols; col++)
+            {
+               Console.Write(board[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 
-    private bool Piece()
+    private bool DropPiece()
     {
-
+        
     }
 
     private bool WinningConditions()
     {
+        // Horizontal
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col <= Cols - 4; col++)
+            {
+                if (board[row, col] == player &&
+                    board[row, col + 1] == player &&
+                    board[row, col + 2] == player &&
+                    board[row, col + 3] == player &&)
+                {
+                    return true;
+                }
+            }
+        }
 
+        // Vertical
+        for (int row = 0; row < Rows - 4; row++)
+        {
+            for (int col = 0; col <= Cols; col++)
+            {
+                if (board[row, col] == player &&
+                    board[row + 1, col] == player &&
+                    board[row + 2, col] == player &&
+                    board[row + 3, col] == player &&)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonally: bottom-left to top-right
+        for (int row = 0; row < Rows - 4; row++)
+        {
+            for (int col = 0; col <= Cols - 4; col++)
+            {
+                if (board[row, col] == player &&
+                    board[row + 1, col + 1] == player &&
+                    board[row + 2, col + 2] == player &&
+                    board[row + 3, col + 3] == player &&)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonally: top-left to bottom-right
+        for (int row = 3; row < Rows; row++)
+        {
+            for (int col = 0; col <= Cols - 4; col++)
+            {
+                if (board[row, col] == player &&
+                    board[row + 1, col + 1] == player &&
+                    board[row + 2, col + 2] == player &&
+                    board[row + 3, col + 3] == player &&)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
     
     class Program
